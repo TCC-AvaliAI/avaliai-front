573ad14763edb585613afb33cc5d783d90d219f8
todpig <luccasraffael6@gmail.com>
2025-06-11 11:47:25 -0300

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,19 @@ const nextConfig: NextConfig = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
